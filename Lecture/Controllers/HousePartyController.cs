f453ea88d364e37fee7b0ce80bc36425124dab0b
autumn-ragland <raglanda998@gmail.com>
2020-09-08 10:26:49 -0500

    retrun content for read and add to DB

--- Lecture/Controllers/HousePartyController.cs
+++ Lecture/Controllers/HousePartyController.cs
@@ -8,9 +8,37 @@ namespace Lecture.Controllers
 {
     public class HouseParty : Controller
     {
-        public IActionResult Index()
+        private readonly InviteContext _context;
+        public HouseParty(InviteContext context)
         {
-            return Content("Sanity");
+            _context = context;
+        }
+        public IActionResult ViewInvite(int inviteID)
+        {
+            Invite matchingInvite = _context.Invites.FirstOrDefault(inv => inv.id == inviteID);
+            if(matchingInvite != null)
+            {
+                return Content($"ID {matchingInvite.id}\nName {matchingInvite.inviteName}\nIs Attending {matchingInvite.isAttending}\nParties Attended {matchingInvite.numberAttended}\nAge : {matchingInvite.age}\n---\n");
+            } else 
+            {
+                return Content("Not matching invite found");
+            }
+        }
+        public IActionResult ListInvites()
+        {
+            string displayStr = "";
+            foreach(Invite inv in _context.Invites)
+            {
+                displayStr += $"ID {inv.id}\nName {inv.inviteName}\nIs Attending {inv.isAttending}\nParties Attended {inv.numberAttended}\nAge : {inv.age}\n---\n";
+            }
+            return Content($"{displayStr}");
+        }
+        [HttpPost]
+        public IActionResult AddInvite([Bind("inviteName", "isAttending", "numberAttended", "age")] Invite newInvite)
+        {
+            _context.Invites.Add(newInvite);
+            _context.SaveChanges();
+            return Content($"Return new invite {newInvite.inviteName}");
         }
     }
 }
