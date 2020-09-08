99742ba46b75d13f0221658bd46281372e7eb57a
autumn-ragland <raglanda998@gmail.com>
2020-09-08 11:22:59 -0500

    simple views

--- Lecture/Controllers/HousePartyController.cs
+++ Lecture/Controllers/HousePartyController.cs
@@ -18,10 +18,14 @@ namespace Lecture.Controllers
             Invite matchingInvite = _context.Invites.FirstOrDefault(inv => inv.id == inviteID);
             if(matchingInvite != null)
             {
-                return Content($"ID {matchingInvite.id}\nName {matchingInvite.inviteName}\nIs Attending {matchingInvite.isAttending}\nParties Attended {matchingInvite.numberAttended}\nAge : {matchingInvite.age}\n---\n");
+                // return Content($"ID {matchingInvite.id}\nName {matchingInvite.inviteName}\nIs Attending {matchingInvite.isAttending}\nParties Attended {matchingInvite.numberAttended}\nAge : {matchingInvite.age}\n---\n");
+                ViewData["invite"] = $"ID {matchingInvite.id}\nName {matchingInvite.inviteName}\nIs Attending {matchingInvite.isAttending}\nParties Attended {matchingInvite.numberAttended}\nAge : {matchingInvite.age}\n---\n";
+                return View();
             } else 
             {
-                return Content("Not matching invite found");
+                // return Content("Not matching invite found");
+                ViewData["error"] = "No matching invite found";
+                return View("Error");
             }
         }
         public IActionResult ListInvites()
@@ -31,14 +35,18 @@ namespace Lecture.Controllers
             {
                 displayStr += $"ID {inv.id}\nName {inv.inviteName}\nIs Attending {inv.isAttending}\nParties Attended {inv.numberAttended}\nAge : {inv.age}\n---\n";
             }
-            return Content($"{displayStr}");
+            // return Content($"{displayStr}");
+            ViewData["invites"] = displayStr;
+            return View();
         }
         [HttpPost]
         public IActionResult AddInvite([Bind("inviteName", "isAttending", "numberAttended", "age")] Invite newInvite)
         {
             _context.Invites.Add(newInvite);
             _context.SaveChanges();
-            return Content($"Return new invite {newInvite.inviteName}");
+            // return Content($"Return new invite {newInvite.inviteName}");
+                ViewData["invite"] = $"ID {newInvite.id}\nName {newInvite.inviteName}\nIs Attending {newInvite.isAttending}\nParties Attended {newInvite.numberAttended}\nAge : {newInvite.age}\n---\n";
+                return View("ViewInvite");
         }
     }
 }
